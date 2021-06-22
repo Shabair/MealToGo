35c15ee408fa80f50d293b49bab6e95758554dd3
Shabair <Shabair143@gmail.com>
2021-06-22 19:01:23 +0500

    Partialy card completed

--- App.js
+++ App.js
@@ -7,8 +7,23 @@ import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant
 import { ThemeProvider } from "styled-components/native";
 
 import { theme } from "./src/infrastructure/theme";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  let [OswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  let [LatoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!OswaldLoaded || !LatoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
